9813ed0359c5a77b79285d538d9db33ea6449a6e
Mara <mara-li@outlook.fr>
2022-07-28 15:59:57 +0200

    chore: downgrade python

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ external, internal = classification_dependencies()
 
 setup(
     name="mkdocs_embed_file_plugins",
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     version=version,
     description="A plugin to quote file from docs",
     author="Mara-Li",
